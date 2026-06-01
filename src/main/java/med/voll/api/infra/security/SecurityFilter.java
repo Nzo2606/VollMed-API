75e22f9bdf04ffff10410d1de5a55228242988cd
Enzo <enzodesouza.queen@gmail.com>
2026-06-01 20:02:22 -0300

    Creation of the class 'SecurityFilter', acting as a filter for the system. Intercepting requisitions and validating JWT tokens

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        var tokenJWT = getToken(request);
+        System.out.println(tokenJWT);
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String getToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null){
+            throw new RuntimeException("JWT Token not sent in the header 'Authorization'! ");
+        }
+        return authorizationHeader.replace("Bearer ", "");
+    }
+}

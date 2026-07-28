06372daa6e088cfb8d0c819ad3844d64ef3ab1ce
Enzo <enzodesouza.queen@gmail.com>
2026-07-28 14:36:59 -0300

    feat: add of the field specialty in the dto class of the appointment scheduling data.

--- src/main/java/med/voll/api/domain/appointment/AppointmentSchedulingData.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentSchedulingData.java
@@ -2,6 +2,7 @@ package med.voll.api.domain.appointment;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.doctor.Specialty;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,8 @@ public record AppointmentSchedulingData(
 
         @NotNull
         @Future
-        LocalDateTime data){
+        LocalDateTime data,
+
+        Specialty specialty){
 
 }

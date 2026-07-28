990f9efed01999104f7fa12b8512ba9aee7f20b9
Enzo <enzodesouza.queen@gmail.com>
2026-07-28 14:47:18 -0300

    feat: addition of methods responsible for taking care of services about the appointments.

--- src/main/java/med/voll/api/domain/appointment/AppointmentService.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentService.java
@@ -1,6 +1,9 @@
 package med.voll.api.domain.appointment;
 
 
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.doctor.Doctor;
 import med.voll.api.domain.doctor.DoctorRepository;
 import med.voll.api.domain.patient.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +24,38 @@ public class AppointmentService {
 
     public void schedule(AppointmentSchedulingData data){
 
+        if (!patientRepository.existsById(data.patientId())){
+            throw new ValidationException("Informed patient ID does not exist!");
+        }
+
+        if (data.doctorId()!= null && !doctorRepository.existsById(data.doctorId())){
+            throw new ValidationException("Informed doctor ID does not exist!");
+        }
+
         var patient = patientRepository.findById(data.patientId()).get();
-        var doctor = doctorRepository.findById(data.doctorId()).get();
+        var doctor = chooseDoctor(data);
         var appointment = new Appointment(null, doctor, patient, data.data());
         appointmentRepository.save(appointment);
     }
 
+    private Doctor chooseDoctor (AppointmentSchedulingData data){
+        if (data.doctorId() != null){
+            return doctorRepository.getReferenceById(data.doctorId());
+        }
 
+        if (data.specialty() == null){
+            throw new ValidationException("Specialty is mandatory when doctor is not choosen");
+        }
+
+        return doctorRepository.chooseRandomDoctorAvailableOnTheDate(data.specialty(), data.data());
+    }
+
+    public void cancel(@Valid AppointmentCancellationData data) {
+        if (!appointmentRepository.existsById(data.appointmentId())){
+            throw new ValidationException("Informed appointment Id does not exist!");
+        }
+
+        var appointment = appointmentRepository.getReferenceById(data.appointmentId());
+        appointment.cancel(data.reason());
+    }
 }

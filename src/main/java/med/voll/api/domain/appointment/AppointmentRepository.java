dc66ba78e7ed51674af8be3010464d061cdfbf71
Enzo <enzodesouza.queen@gmail.com>
2026-07-28 17:20:07 -0300

    feat: creation of the method 'existsByDoctorIdAndData', responsible for confirming if a doctor has a schedule in a certain time slot. Using Spring Data JPA as its main method of query.

--- src/main/java/med/voll/api/domain/appointment/AppointmentRepository.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentRepository.java
@@ -3,4 +3,5 @@ package med.voll.api.domain.appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    Boolean existsByDoctorIdAndData(Long aLong, @NotNull @Future LocalDateTime data);
 }

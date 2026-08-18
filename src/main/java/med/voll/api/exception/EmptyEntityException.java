89ad634450c3ff31faf602f8dc5eb832e5e31a91
Enzo <enzodesouza.queen@gmail.com>
2026-08-17 23:03:34 -0300

    feat: creation of personalized exception class `EmptyEntityException`. Being thrown when an empty entity is searched

---
+++ src/main/java/med/voll/api/exception/EmptyEntityException.java
@@ -0,0 +1,8 @@
+package med.voll.api.exception;
+
+public class EmptyEntityException extends RuntimeException {
+
+    public EmptyEntityException(String message) {
+        super(message);
+    }
+}
